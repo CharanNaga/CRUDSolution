8c1b1076279892e5ed87654a09e60d56a6634113
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-07 17:34:06 +0530

    Defined a method in interface and added documentation comments.

--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -1,7 +1,17 @@
-﻿namespace ServiceContracts
-{
-    public class ICountriesService
-    {
+﻿using ServiceContracts.DTO;
 
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents business logic for manipulating Country entity.
+    /// </summary>
+    public interface ICountriesService
+    {
+        /// <summary>
+        /// Adds a Country Object to the list of Countries.
+        /// </summary>
+        /// <param name="countryAddRequest">Country object to be added.</param>
+        /// <returns>Returns Country Object after adding it (including newly generated Country Id)</returns>
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
     }
 }
