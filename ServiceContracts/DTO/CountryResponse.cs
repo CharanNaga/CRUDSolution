b4aa95e53018dbfef65a069522eca6536e76ae73
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-07 21:34:48 +0530

    Overridden Equals() in CountryResponse.cs

--- ServiceContracts/DTO/CountryResponse.cs
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -11,6 +11,21 @@ namespace ServiceContracts.DTO
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
         //As per business logic, there is a need for converting Country object into country response.
+
+        //It compares current object to another object of CountryResponse type and returns true if both values are same, otherwise false.
+        public override bool Equals(object? obj)
+        {
+            if(obj == null) 
+                return false;
+            if (obj.GetType() != typeof(CountryResponse))
+                return false;
+            CountryResponse countryToCompare = (CountryResponse)obj;
+            return CountryID == countryToCompare.CountryID && CountryName == countryToCompare.CountryName;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
     //creating extension method for converting the country obj into country response obj.
     public static class CountryExtensions
