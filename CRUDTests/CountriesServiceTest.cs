0c24dea73910862a54b4680417215ec3b67a2a98
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-07 17:44:13 +0530

    Created a new Test for CountriesService

---
+++ CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Entities;
+using System;
+using System.Collections.Generic;
+
+namespace CRUDTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+    }
+}
