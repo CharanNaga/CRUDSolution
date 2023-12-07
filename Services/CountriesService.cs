8a3419afdbb9665aa5a5bddea24e52e08fbe0ad3
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-07 18:50:04 +0530

    Written correct implementation for AddCountry() making all test cases passed.

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,13 +1,43 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>(); 
+        }
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            //1. Check whether countryAddRequest != null
+            if(countryAddRequest == null)
+                throw new ArgumentNullException(nameof(countryAddRequest));
+
+            //2. Validate all properties of countryAddRequest
+            //CountryName can't be null.
+            if(countryAddRequest.CountryName == null)
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+
+            //CountryName can't be duplicated.
+            if (_countries.Where(c=>c.CountryName == countryAddRequest.CountryName).Count() > 0)
+                throw new ArgumentException("Country Name already existed");
+
+            //3. Convert countryAddRequest from CountryAddRequest type to Country type.
+            Country country = countryAddRequest.ToCountry(); //Entities(Domain Models) should hide from Controller or Unit test classes.
+
+            //4. Generate new CountryID
+            country.CountryID = Guid.NewGuid();
+
+            //5. Add to List<Country>
+            _countries.Add(country);
+
+            //6. Return CountryResponse object with generated CountryID.
+            return country.ToCountryResponse();
         }
     }
 }
