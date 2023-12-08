17b33949d6d05df22fb3833da68da6e43b5d3dc2
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-08 15:53:31 +0530

    Created a new interface IPersonsService.cs

---
+++ ServiceContracts/IPersonsService.cs
@@ -0,0 +1,28 @@
+﻿using ServiceContracts.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents business logic for manipulating Person entity.
+    /// </summary>
+    public interface IPersonsService
+    {
+        /// <summary>
+        /// Adds new person into list of persons
+        /// </summary>
+        /// <param name="personAddRequest">Person to add</param>
+        /// <returns>Returns same person details along with newly generated PersonID</returns>
+        PersonResponse AddPerson(PersonAddRequest personAddRequest);
+
+        /// <summary>
+        /// Returns all Persons
+        /// </summary>
+        /// <returns>Returns a list of objects of PersonResponse type</returns>
+        List<PersonResponse> GetAllPersons();
+    }
+}
