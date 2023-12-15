c3b90493be45ffbd01d6431e69e512870c4b9d87
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-15 17:22:05 +0530

    Changing Services in Program.cs from Singleton to Scoped

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -7,11 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //add services into IoC Container
-builder.Services.AddSingleton<ICountriesService, CountriesService>();
-builder.Services.AddSingleton<IPersonsService, PersonsService>();
+//builder.Services.AddSingleton<ICountriesService, CountriesService>();
+//builder.Services.AddSingleton<IPersonsService, PersonsService>();
+
+//After we change all the methods in service methods to perform operations with datastore (from in-memory collections to Database),
+//we'll get an error saying can't consume scoped service( entities) from singleton service(services class).
+//so, we'llconvert the lifetimes of Countries & Persons Services to Scoped Service Lifetime.
+
+builder.Services.AddScoped<ICountriesService, CountriesService>();
+builder.Services.AddScoped<IPersonsService, PersonsService>();
 
 //adding DbContext as a service
-builder.Services.AddDbContext<PersonsDbContext>(
+builder.Services.AddDbContext<PersonsDbContext>( //by default scoped service.
     options =>
     {
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
