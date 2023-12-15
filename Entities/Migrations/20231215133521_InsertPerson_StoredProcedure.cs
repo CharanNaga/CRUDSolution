2f3b4a8dbdb6a7241522627a3c43dfab7c00f795
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-15 19:21:48 +0530

    Written code for creating & dropping proc inside Up & Down() in InsertPerson_StoredProc in migrations

--- Entities/Migrations/20231215133521_InsertPerson_StoredProcedure.cs
+++ Entities/Migrations/20231215133521_InsertPerson_StoredProcedure.cs
@@ -8,12 +8,23 @@ namespace Entities.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            string sp_InsertPerson = @"
+                CREATE PROCEDURE [dbo].[InsertPerson]
+                (@PersonID uniqueidentifier, @PersonName nvarchar(40), @Email nvarchar(40), @DateOfBirth datetime2(7), @Gender nvarchar(10), @CountryID uniqueidentifier, @Address nvarchar(200), @ReceiveNewsLetters bit)
+                AS BEGIN
+                    INSERT INTO [dbo].[Persons](PersonID,PersonName,Email,DateOfBirth,Gender,CountryID,Address,ReceiveNewsLetters)
+                    VALUES(@PersonID,@PersonName,@Email,@DateOfBirth,@Gender,@CountryID,@Address,@ReceiveNewsLetters)
+                END
+            ";
+            migrationBuilder.Sql(sp_InsertPerson);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            string sp_InsertPerson = @"
+                DROP PROCEDURE [dbo].[InsertPerson]
+            ";
+            migrationBuilder.Sql(sp_InsertPerson);
         }
     }
 }
