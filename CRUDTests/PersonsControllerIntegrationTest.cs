dc14a0d0349f5190454b5a7ba9e77f39c012de1f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-19 20:28:16 +0530

    Modified IntegrationTest class to read response body and assert using Fizzler package

--- CRUDTests/PersonsControllerIntegrationTest.cs
+++ CRUDTests/PersonsControllerIntegrationTest.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using FluentAssertions;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc.Localization;
 using System.Net.Http;
 
 namespace CRUDTests
@@ -23,8 +26,17 @@ namespace CRUDTests
 
             //Assert
             responseMessage.Should().BeSuccessful(); //2xx status code
-        }
 
+            string responseBody = await responseMessage.Content.ReadAsStringAsync();
+
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(responseBody);
+
+            var document = html.DocumentNode;
+
+            //with this, we can write asserts like: if textbox value exists or not, validation error message displayed or not, submit button exists or not with the help of Fizzler
+            document.QuerySelectorAll("table.persons").Should().NotBeNull(); //table tag with css class persons
+        }
         #endregion
     }
 }
