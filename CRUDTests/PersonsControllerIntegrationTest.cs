d2697ea4087986a8d32867ffcd8e98ce72225370
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-19 19:25:30 +0530

    Modified PersonsControllerIntegrationTest.cs to make use of CustomWebApplicationFactory & configure httpClient to get ResponseMessage

--- CRUDTests/PersonsControllerIntegrationTest.cs
+++ CRUDTests/PersonsControllerIntegrationTest.cs
@@ -3,19 +3,26 @@ using System.Net.Http;
 
 namespace CRUDTests
 {
-    public class PersonsControllerIntegrationTest
+    public class PersonsControllerIntegrationTest: IClassFixture<CustomWebApplicationFactory> //IClassFixture<> provides the object of CustomWebApplicationFactory
     {
+        private readonly HttpClient _httpClient;
+
+        public PersonsControllerIntegrationTest(CustomWebApplicationFactory factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
         #region Index
         [Fact]
-        public void Index_ToReturnView()
+        public async Task Index_ToReturnView()
         {
             //Arrange
 
             //Act
-            HttpResponseMessage responseMessage = _client.GetAsync("/Persons/Index");
+            HttpResponseMessage responseMessage = await _httpClient.GetAsync("/Persons/Index");
 
             //Assert
-            responseMessage.Should().BeSuccessful();
+            responseMessage.Should().BeSuccessful(); //2xx status code
         }
 
         #endregion
