74063865700ddc0b27d4ecec2176e60d991f55ad
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-19 18:28:04 +0530

    Created PersonsControllerIntegrationTest.cs to do testing using HttpRequest object at all stages of layers

---
+++ CRUDTests/PersonsControllerIntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System.Net.Http;
+
+namespace CRUDTests
+{
+    public class PersonsControllerIntegrationTest
+    {
+        #region Index
+        [Fact]
+        public void Index_ToReturnView()
+        {
+            //Arrange
+
+            //Act
+            HttpResponseMessage responseMessage = _client.GetAsync("/Persons/Index");
+
+            //Assert
+            responseMessage.Should().BeSuccessful();
+        }
+
+        #endregion
+    }
+}
