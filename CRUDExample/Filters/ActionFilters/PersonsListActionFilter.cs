766e5ee56b96e98d7bafa9cc54c94b030e2fe298
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-21 23:22:47 +0530

    Injected ILogger & implemented ActionFilter class with logger information in both methods

--- CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -1,6 +1,24 @@
-﻿namespace CRUDExample.Filters.ActionFilters
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ActionFilters
 {
-    public class PersonsListActionFilter
+    public class PersonsListActionFilter : IActionFilter
     {
+        private readonly ILogger<PersonsListActionFilter> _logger;
+        public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+        {
+            _logger = logger;
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //Add "after execution" logic here
+            _logger.LogInformation($"{nameof(PersonsListActionFilter)}.{nameof(OnActionExecuted)} Filter method");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            //Add "before execution" logic here
+            _logger.LogInformation($"{nameof(PersonsListActionFilter)}.{nameof(OnActionExecuting)} Filter method");
+        }
     }
 }
