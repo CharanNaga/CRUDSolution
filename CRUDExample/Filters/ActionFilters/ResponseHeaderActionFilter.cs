4f51b90fcb70cbd099723162a37136ea37fd5cdc
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-27 12:57:54 +0530

    Modified ResponseHeader class by injecting ILogger & written information message in both methods

--- CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -4,15 +4,25 @@ namespace CRUDExample.Filters.ActionFilters
 {
     public class ResponseHeaderActionFilter : IActionFilter
     {
+        
+        private readonly ILogger<ResponseHeaderActionFilter> _logger;
+
+        //injecting ILogger
+        public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger)
+        {
+            _logger = logger;
+        }
         //after execution of Action method
         public void OnActionExecuted(ActionExecutedContext context)
         {
+            _logger.LogInformation("{FilterName}.{MethodName} method",nameof(ResponseHeaderActionFilter),nameof(OnActionExecuted));
             throw new NotImplementedException();
         }
 
         //before execution of Action Method
         public void OnActionExecuting(ActionExecutingContext context)
         {
+            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(ResponseHeaderActionFilter), nameof(OnActionExecuting));
             throw new NotImplementedException();
         }
     }
