7f72305e300c314614c62483415647a9759f0ae2
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-27 12:52:05 +0530

    Created new ResponseHeader class in ActionFilters folder & implemented IActionFilter with dummy logic

---
+++ CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ActionFilters
+{
+    public class ResponseHeaderActionFilter : IActionFilter
+    {
+        //after execution of Action method
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        //before execution of Action Method
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
