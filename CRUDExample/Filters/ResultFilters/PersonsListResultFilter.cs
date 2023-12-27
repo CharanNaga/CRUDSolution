7313cc17e219d299674cd65bea5f25f325abc078
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-27 20:21:48 +0530

    Created PersonsListResultFilter under Filters/ResultFilters to deal with IActionResult on before & after

---
+++ CRUDExample/Filters/ResultFilters/PersonsListResultFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ResultFilters
+{
+    public class PersonsListResultFilter : IAsyncResultFilter
+    {
+        private readonly ILogger<PersonsListResultFilter> _logger;
+        public PersonsListResultFilter(ILogger<PersonsListResultFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            //before execution
+            _logger.LogInformation("{FilterName}.{MethodName} - before execution",nameof(PersonsListResultFilter),nameof(OnResultExecutionAsync));
+
+            await next(); //call the subsequent filter or IActionResult
+
+            //after execution
+            _logger.LogInformation("{FilterName}.{MethodName} - after execution", nameof(PersonsListResultFilter), nameof(OnResultExecutionAsync));
+            context.HttpContext.Response.Headers["Last-Modified"] = DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+        }
+    }
+}
