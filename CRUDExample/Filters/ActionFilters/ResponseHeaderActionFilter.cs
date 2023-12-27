0cc0bff34916e03ced02b3ef7bd7a21a9e2143fa
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-27 13:52:29 +0530

    Modified ResponseHeader class to accept two parameters to work like a Parameterized ActionFilter

--- CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -2,28 +2,34 @@
 
 namespace CRUDExample.Filters.ActionFilters
 {
+    //Parameterized Action Filter as it receives args key & value
     public class ResponseHeaderActionFilter : IActionFilter
     {
         
         private readonly ILogger<ResponseHeaderActionFilter> _logger;
+        private readonly string Key;
+        private readonly string Value;
 
         //injecting ILogger
-        public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger)
+        public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger, string key, string value)
         {
             _logger = logger;
+            Key = key;
+            Value = value;
         }
         //after execution of Action method
         public void OnActionExecuted(ActionExecutedContext context)
         {
             _logger.LogInformation("{FilterName}.{MethodName} method",nameof(ResponseHeaderActionFilter),nameof(OnActionExecuted));
-            throw new NotImplementedException();
+            context.HttpContext.Response.Headers[Key] = Value;
+
         }
 
         //before execution of Action Method
         public void OnActionExecuting(ActionExecutingContext context)
         {
             _logger.LogInformation("{FilterName}.{MethodName} method", nameof(ResponseHeaderActionFilter), nameof(OnActionExecuting));
-            throw new NotImplementedException();
+
         }
     }
 }
