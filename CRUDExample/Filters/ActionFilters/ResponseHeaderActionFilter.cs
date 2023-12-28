5ab4653178aeffcd03690eebd979bd283a04f095
CharanNaga <nagasaicharansinghbondili@gmail.com>
2023-12-28 20:02:38 +0530

    Modified ResponseHeaderActionFilter class to make use of parent class ActionFilterAttribute

--- CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -3,33 +3,22 @@
 namespace CRUDExample.Filters.ActionFilters
 {
     //Parameterized Action Filter as it receives args key & value
-    public class ResponseHeaderActionFilter : IAsyncActionFilter,IOrderedFilter
+    public class ResponseHeaderActionFilter : ActionFilterAttribute
     {
-        
-        private readonly ILogger<ResponseHeaderActionFilter> _logger;
         private readonly string _key;
         private readonly string _value;
 
-        public int Order { get; set; }
-
         //injecting ILogger
-        public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger, string key, string value, int order)
+        public ResponseHeaderActionFilter(string key, string value, int order)
         {
-            _logger = logger;
             _key = key;
             _value = value;
             Order = order;
         }
 
-        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            //before execution
-            _logger.LogInformation("{FilterName}.{MethodName} method - before", nameof(ResponseHeaderActionFilter), nameof(OnActionExecutionAsync));
-
             await next(); //calls subsequent filter or action method
-
-            //after execution
-            _logger.LogInformation("{FilterName}.{MethodName} method - after", nameof(ResponseHeaderActionFilter), nameof(OnActionExecutionAsync));
             context.HttpContext.Response.Headers[_key] = _value;
         }
     }
